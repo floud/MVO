09b449ec5f8cd2ce87b3dc6b5fecbc20c1a5db71
floud <ahalturin@bk.ru>
2014-09-11 14:23:00 +0400

    third one

--- src/ru/floud/Plane.java
+++ src/ru/floud/Plane.java
@@ -17,11 +17,18 @@ public class Plane {
         return ((b.getX() - a.getX()) * (c.getY() - b.getY()) - (b.getY() - a.getY()) * (c.getX() - b.getX()));
     }
 
+    //Измерение длины отрезка, состоящего из двух точек.
+    public static double segmentLength(Point one, Point two)
+    {
+        return Math.sqrt(Math.pow((two.getX() - one.getX()), 2) + Math.pow((two.getY() - one.getY()), 2));
+    }
+
     public void addPoint(Point point) {
         points.add(point);
     }
 
-    public void printMvo() {
+    public void printMvo()
+    {
 
 
         ArrayList<Point> copy = new ArrayList<Point>(points);
@@ -31,7 +38,8 @@ public class Plane {
         double min = Double.MAX_VALUE;
         for (Point point : points)
         {
-            if (point.getX() < min) {
+            if (point.getX() < min)
+            {
                 min = point.getX();
                 left = point;
                 list.add(points.indexOf(left));
@@ -42,23 +50,47 @@ public class Plane {
         copy.set(copy.size() - 1, left);
 
         Point current = left;
-        for (int i = 0; i < copy.size(); i++) {
+        for (int i = 0; i < copy.size(); i++)
+        {
             Point b = copy.get(i);
-            if (list.contains(points.indexOf(b))) continue;
-            for (int j = 0; j < copy.size(); j++) {
+            if (b == current) continue;
+            //if (list.contains(points.indexOf(b))) continue;
+            for (int j = 0; j < copy.size(); j++)
+            {
                 Point c = copy.get(j);
-                //if (c == current || c == b) continue;
-                if (rotate(current, b, c) < 0) {
+                double angle = rotate(current, b, c); //результат скалярного произведения векторов
+                if (c == b) continue;
+                if (angle > 0 || angle == 0 && (segmentLength(current, b) < segmentLength(current, c)))
+                {
                     b = c;
                 }
+
             }
             if (b == left) break;
-            else {
+            else
+            {
                 list.add(this.points.indexOf(b));
                 current = b;
                 //copy.remove(b);
             }
         }
+        /*int i = 0;
+        while (true) {
+            Point b = copy.get(i);
+            for (int j = 0; j < copy.size(); j++) {
+                Point c = copy.get(j);
+                if (c == b) continue;
+                if (rotate(current, b, c) > 0) {
+                    b = c;
+                }
+            } if (b == left) break;
+            else {
+                list.add(this.points.indexOf(b));
+                current = b;
+            }
+            i++;
+
+        }*/
         for (int ints : list)
         {
             System.out.printf(ints + 1 + " ");
@@ -66,12 +98,19 @@ public class Plane {
     }
 
     //Загрузка точек на плоскость
-    public void loadPlane() throws IOException {
+    public void loadPlane() throws IOException
+    {
         this.points.add(new Point(2, 3));
         this.points.add(new Point(4, 4));
-        this.points.add(new Point(4, 7));
+        this.points.add(new Point(3, 7));
         this.points.add(new Point(6, 5));
         this.points.add(new Point(7, 2));
+        this.points.add(new Point(5, 7));
+        this.points.add(new Point(5, 2));
+        this.points.add(new Point(3, 5));
+        this.points.add(new Point(4, 6));
+        this.points.add(new Point(6, 2));
+        this.points.add(new Point(0, 8));
 
         /*BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(WELCOME_STRING);
@@ -105,3 +144,5 @@ public class Plane {
         //}
     }
 }
+
+
--- src/ru/floud/Solution_MVO.java
+++ src/ru/floud/Solution_MVO.java
@@ -6,13 +6,14 @@ import java.io.IOException;
 /**
  * Created by halturin_a on 05.09.2014.
  */
-public class Solution_MVO {
-    public static void main(String[] args) throws IOException {
+public class Solution_MVO
+{
+    public static void main(String[] args) throws IOException
+    {
         //создаем плоскость и наносим на нее точки с координатами х и y
         Plane plane = new Plane();
         plane.loadPlane();
         plane.printMvo();
-
 
     }
 }
