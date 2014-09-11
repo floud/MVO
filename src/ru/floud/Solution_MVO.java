2294fcb2982a17a57b023a1c61f4dc90fab7b066
floud <ahalturin@bk.ru>
2014-09-11 20:49:13 +0400

    Alpha version

--- src/ru/floud/Solution_MVO.java
+++ src/ru/floud/Solution_MVO.java
@@ -16,6 +16,7 @@ public class Solution_MVO
     private static final String ENTER_X = "Введите х:";
     private static final String ENTER_Y = "Введите y:";
     private static final String EXIT = "exit";
+    private static final String WARNING = "Построение МВО невозможно: точек меньше 3-х или они на одной прямой";
     private static ArrayList<Point> points = new ArrayList<Point>(); //массив точек на плоскости
     private static ArrayList<Integer> list = new ArrayList<Integer>(); //массив с индексами точек-вершин в МВО
 
@@ -23,6 +24,7 @@ public class Solution_MVO
     {
         loadPoints();
         printMvo();
+
     }
 
     //Проверяет с какой стороны луча "ab" на ходится точка "с"
@@ -42,22 +44,26 @@ public class Solution_MVO
     //Выводит на экран точки-вершины МВО
     public static void printMvo()
     {
+        if (!checkPossibilityMvo()) {
+            System.out.println(WARNING);
+            return;
+        }
         ArrayList<Point> copy = new ArrayList<Point>(points); //список-копия для манипуляций с расположением точек в массиве, не затрагивая оригинал.
 
         //Поиск самой левой нижней точки (первая вершина МВО).
         Point left = null;
         double minX = Double.MAX_VALUE;
         double minY = Double.MIN_VALUE;
-        for (Point point : points)
+        for (Point p : points)
         {
-            if (point.getX() < minX || point.getX() == minX && point.getY() < minY)
+            if (p.getX() < minX || p.getX() == minX && p.getY() < minY)
             {
-                minX = point.getX();
-                minY = point.getY();
-                left = point;
+                minX = p.getX();
+                minY = p.getY();
+                left = p;
             }
         }
-        list.add(points.indexOf(left));
+        list.add(points.indexOf(left)); //левая нижняя вершина добавляется первой в список индексов вершин
 
         //Найденную точку и последнюю в списке меняем местами для выхода из цикла при попадании на этот элемент (цикл сделает круг по всем точкам)
         copy.set(copy.indexOf(left), copy.get(copy.size() - 1));
@@ -65,17 +71,14 @@ public class Solution_MVO
 
         //"Проход" по всем точкам в поисках точек-вершин
         Point current = left;
-        for (int i = 0; i < copy.size(); i++)
+
+        for (Point b : copy)
         {
-            Point b = copy.get(i);
             if (b == current) continue;
-            for (int j = 0; j < copy.size(); j++)
-            {
-                Point c = copy.get(j);
+            for (Point c : copy) {
                 double angle = rotate(current, b, c);
                 if (c == b) continue;
-                if (angle > 0 || angle == 0 && (segmentLength(current, b) < segmentLength(current, c)))
-                {
+                if (angle > 0 || angle == 0 && (segmentLength(current, b) < segmentLength(current, c))) {
                     b = c;
                 }
             }
@@ -86,6 +89,7 @@ public class Solution_MVO
                 current = b;
             }
         }
+        //Вывод индексов вершин на экран с инкрементацией
         for (int ints : list)
         {
             System.out.printf(ints + 1 + " ");
@@ -140,5 +144,15 @@ public class Solution_MVO
     public static void addPoint(Point point)
     {
         points.add(point);
+    }
+
+    //Проверяет возможность построения МВО из существующих точек. Если точек < 3 или все точки лежат на одной прямой построение невозможно.
+    public static boolean checkPossibilityMvo() {
+        if (points.size() < 3) return false;
+        int count = 0;
+        for (int i = 1; i < points.size(); i++) {
+            if (rotate(points.get(0), points.get(1), points.get(i)) != 0) count++;
+        }
+        return count != 0;
     }
 }
