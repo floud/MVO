e197e3d1882eff06a5ed5ad986c4db834d7fb5ea
floud <ahalturin84@gmail.com>
2014-09-11 21:15:03 +0400

    final

--- src/ru/floud/Solution_MVO.java
+++ src/ru/floud/Solution_MVO.java
@@ -155,4 +155,23 @@ public class Solution_MVO
         }
         return count != 0;
     }
+
+
+    public static class Point {
+        private double x;
+        private double y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+    }
 }
